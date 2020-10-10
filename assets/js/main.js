6b58ca2988d56ba9f62c651c26ecdaa5a2b5c4b4
Milon9787 <56488424+Milon9787@users.noreply.github.com>
2020-10-10 23:42:44 +0600

    slider start

--- assets/js/main.js
+++ assets/js/main.js
@@ -149,6 +149,45 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 }
 
+
+
+
+
+
+/* BS form Validator*/
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
+
+
+
+
+/*----- end of Rannojit -----*/
+
+
+
+/*----- end of Noyon -----*/
+
+
+
+/*----- end of Shariful -----*/
+
+
 /*
 -----------------------
 Start event Google Map ->> 
@@ -188,46 +227,45 @@ if( $('#eventgooglemap').length ){
     google.maps.event.addDomListener(window, 'load', initialize);
 }
 
-
-
-/*----end of Milon-----*/
-
-
-
-/* BS form Validator*/
-(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
+if( $('.relevant-evnt-slider').length ){
+    $('.relevant-evnt-slider').slick({
+      dots: false,
+      infinite: false,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
         }
-        form.classList.add('was-validated');
-      }, false);
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     });
-  }, false);
-})();
-
-
-
-
-
-/*----- end of Rannojit -----*/
-
-
-
-/*----- end of Noyon -----*/
-
-
-
-/*----- end of Shariful -----*/
-
-
+}
 
 /*----- end of Milon -----*/
 
